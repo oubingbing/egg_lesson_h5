ebc2ed8d6b29b5eee6d9cecd8034a644f69fd9f8
Falcon <5394918+falcon_wang@user.noreply.gitee.com>
2023-04-12 18:42:39 +0800

    update

--- public/js/tool/wx.js
+++ public/js/tool/wx.js
@@ -22,7 +22,7 @@ function getSignature(res) {
 function setApi(title, desc, imgUrl) {
     wx.ready(function() { //需在用户可能点击分享按钮前就先调用
         wx.checkJsApi({
-            jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+            jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData', 'onMenuShareTimeline', 'onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareQZone'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
             success: function(res) {
                 // 以键值对的形式返回，可用的api值true，不可用为false
                 console.log("====res", res);
