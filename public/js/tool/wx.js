5d2fd04170946519deeeba604bcac668a17caf83
falcon_workpc <falcon_workpc@work.com>
2023-04-12 17:02:40 +0800

    update

--- public/js/tool/wx.js
+++ public/js/tool/wx.js
@@ -41,6 +41,7 @@ function setApi(title,desc,imgUrl){
       // if(res.checkResult.updateAppMessageShareData){
       wx.updateAppMessageShareData({ 
           title, // 分享标题
+          desc, // 分享描述
           link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
           imgUrl, // 分享图标
           success: function () {
