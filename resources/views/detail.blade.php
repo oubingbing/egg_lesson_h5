58de88a8165ae2eff5b8e7e79f898140d106c63d
falcon_workpc <falcon_workpc@work.com>
2023-04-17 16:51:37 +0800

    update detail.html to mode

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -65,11 +65,9 @@
         <div class="part1 anchor1" id="part1">
             <div class="line1">
 
-                @if ($goods_detail["reset_title"])
+                
                 {{$goods_detail["reset_title"]}}
-                @else
-                无标题
-                @endif
+                
             </div>
             <div class="line15">
                 <div class="item"></div>
