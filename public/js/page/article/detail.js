861bc04ee36eb077415df76ce737aee07700eb1a
Falcon <5394918+falcon_wang@user.noreply.gitee.com>
2023-05-05 22:25:40 +0800

    update

--- public/js/page/article/detail.js
+++ public/js/page/article/detail.js
@@ -1,4 +1,5 @@
 var state = {
+    swiper:null,
     connect_articles: [
         { title: "如何成为一名王者大神", seo_describe: "如何如何如何如何所以所以这般这般" },
         { title: "如何成为一名王者中神", seo_describe: "如何如何如何如何所以所以这般这般" },
@@ -8,17 +9,17 @@ var state = {
 };
 
 function setConnectArticles() {
-    let items = state.connect_articles;
-    if (items.length) {
-        for (let j in items) {
-            let item = document.createElement("div");
-            item.className = "item";
-            item.innerHTML = `<div class="bg"></div>
-                            <div class="name">${items[j].title}</div>
-                            <div class="description">${items[j].seo_describe}</div>`;
-            document.getElementById(`list_1_items`).appendChild(item);
-        }
-    }
+ state.swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    centeredSlides: true,
+    autoplay:true,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  });
 }
 $(document).ready(() => {
     this.setConnectArticles();
