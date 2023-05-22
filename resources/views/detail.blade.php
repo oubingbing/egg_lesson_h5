ad0088d6aed8680d9149bac3cdac077fb1eb1fc2
falcon_workpc <falcon_workpc@work.com>
2023-05-22 15:16:25 +0800

    udpate

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -378,6 +378,20 @@
                 </div>
             </div>
 
+            <div class="other-page">
+                @if ($goods_detail['previous'] && $goods_detail['pre']['title'])
+                <div class="btn"><span>上一篇：</span><a onclick="goTo('detail','id',{{$goods_detail['previous']['id']}})">{{$article['previous']['title']}}</a></div>
+                @else
+                <div class="btn"><span>上一篇：</span><a>没有了</a></div>
+                @endif
+                @if ($goods_detail['next'] && $goods_detail['next']['title'])
+                <div class="btn"><span>下一篇：</span><a onclick="goTo('goods_detail','id',{{$goods_detail['next']['id']}})">{{$goods_detail['next']['title']}}</a></div>
+                @else
+                <div class="btn"><span>下一篇：</span><a>没有了</a></div>
+                @endif
+            </div>
+            
+
             <div class="hot-goods anchor4">
                 <div class="title">
                     <div class="left">热门推荐
